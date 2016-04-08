3d10bf477d57df1afe5edf1d9489eb6d7e69e8ce
Aditya Thota <aditya.thota@yuvawallet.com>
2016-04-08 15:14:44 +0530

    Gradle Project Structure

---
+++ src/main/java/org/adicoin/core/Address.java
@@ -0,0 +1,10 @@
+package org.adicoin.core;
+
+public class Address {
+	
+	public boolean someLibraryMethod() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
---
+++ src/test/java/AddressTest.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+import org.adicoin.core.Address;
+
+public class AddressTest {
+    @Test public void testSomeLibraryMethod() {
+        Address classUnderTest = new Address();
+        assertTrue("someLibraryMethod should return 'true'", classUnderTest.someLibraryMethod());
+    }
+}
