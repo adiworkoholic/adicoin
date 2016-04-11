91da646d3dbdf77d56e0c9d4c938de39e10082a6
Aditya Thota <aditya.thota@yuvawallet.com>
2016-04-11 11:05:19 +0530

    Comment on Block Header.

--- src/main/java/org/adicoin/core/Block.java
+++ src/main/java/org/adicoin/core/Block.java
@@ -9,6 +9,20 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+/**
+ * 	02000000 ........................... Block version: 2
+
+	b6ff0b1b1680a2862a30ca44d346d9e8
+	910d334beb48ca0c0000000000000000 ... Hash of previous block's header
+	9d10aa52ee949386ca9385695f04ede2
+	70dda20810decd12bc9b048aaab31471 ... Merkle root
+
+	24d95a54 ........................... Unix time: 1415239972
+	30c31b18 ........................... Target: 0x1bc330 * 256**(0x18-3)
+	fe9f0864 ........................... Nonce
+ * @author aditya
+ *
+ */
 public class Block extends Message {
     private static final Logger log = LoggerFactory.getLogger(Block.class);
 
